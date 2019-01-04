4b72b3682432575e80d8f4bff7a4bdc131eacc93
vibhusomeshwar <redwallguy@gmail.com>
2019-01-04 16:57:04 -0500

    Made clip searchable in admin alias add page, added board name to admin clip display

--- soundboardsite/control/admin.py
+++ soundboardsite/control/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import *
 
+admin.site.site_header =  "Milton Administration. No posers allowed."
+admin.site.site_title = "Milton Administration. No posers allowed."
+admin.site.index_title = ""
+
 @admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
     search_fields=['name']
@@ -9,9 +13,11 @@ class BoardAdmin(admin.ModelAdmin):
 @admin.register(Clip)
 class ClipAdmin(admin.ModelAdmin):
     search_fields=['name','board__name']
+    list_display = ("name", "board") #shows name as well as board name on clip
 
 @admin.register(Alias)
 class AliasAdmin(admin.ModelAdmin):
     search_fields=['name','clip__name']
+    raw_id_fields = ('clip',) #used to have clip show as input element rather than scroll-select box
 
 # Register your models here.
